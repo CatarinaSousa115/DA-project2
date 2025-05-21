6ea8bb6920b787d56dc87878e7a983a70bf3aa9a
CatarinaSousa115 <up202306419@edu.fe.up.pt>
2025-05-21 02:34:54 +0100

    fix brute-force

--- code/algorithms/brute_force.cpp
+++ code/algorithms/brute_force.cpp
@@ -14,16 +14,18 @@
  * @param truck The truck object containing the capacity and number of pallets.
  * @param pallets A vector of Pallet objects available to choose from.
  *
- * @note Time complexity: O(n × 2^n), where n is the number of pallets.
+ * @note Time complexity: O(2^n), where n is the number of pallets.
  * @note Space complexity: O(n), where n is the number of pallets (due to the subset vectors).
  */
+
+ 
 void bruteForce(const Truck &truck, const std::vector<Pallet> &pallets)
 {
     std::cout << "\n=== Brute-force Algorithm ===\n";
     auto start = std::chrono::high_resolution_clock::now();
     char choice;
     /// Warns the user if the number of pallets is too high for brute-force and prompts for confirmation.
-    if (truck.numPallets >= 20)
+    if (truck.numPallets > 30)
     {
         std::cout << "The number of pallets is too high for the brute-force algorithm.\n";
         std::cout << "You really want to use this algorithm? (y/n): ";
@@ -47,45 +49,18 @@ void bruteForce(const Truck &truck, const std::vector<Pallet> &pallets)
         }
     }
 
-    /// Stores the maximum profit found among all valid subsets.
-    int max = 0;
-    
-    // Number of total combinations = 2^n (each pallet can be included or not)
-    int numSubSet = pow(2, truck.numPallets);
-    std::vector<Pallet> bestPallets;
-
-    /**
-     * @brief Calculates all possible subsets and checks which ones are valid under the truck's capacity.
-     *
-     * Iterates through each subset represented by an integer bitmask, calculates the total weight and profit,
-     * and updates the best solution if criteria are met.
-     */
-    for (int i = 0; i < numSubSet; i++)
+    int maxWeight = truck.capacity;
+    if (pallets.empty() || maxWeight <= 0)
     {
-        /// Builds a subset of pallets and calculates its total weight and profit.
-        int totalWeight = 0;
-        int totalProfit = 0;
-        std::vector<Pallet> selectedPallets;
-
-        for (int j = 0; j < truck.numPallets; j++)
-        {
-            // Checks if pallet j is included in the current subset (i's bitmask)
-            if (i & (1 << j))
-            {
-                totalWeight += pallets[j].weight;
-                totalProfit += pallets[j].profit;
-                selectedPallets.push_back(pallets[j]);
-            }
-        }
-
-        /// Updates the best solution if the current subset is valid and more profitable or equally profitable but smaller.
-        if (totalWeight <= truck.capacity &&
-            (totalProfit > max || (totalProfit == max && bestPallets.size() > selectedPallets.size())))
-        {
-            max = totalProfit;
-            bestPallets = selectedPallets;
-        }
+        std::cout << "No pallets available or 0 capacity" << std::endl;
+        return;
     }
+
+    int maxProfit = 0;
+    std::vector<Pallet> bestPallets;
+    std::vector<Pallet> currentPallets;
+
+    backtrack(pallets, maxWeight, 0, 0, 0, currentPallets, bestPallets, maxProfit);
 
     // If no valid subset was found (i.e., all exceed truck capacity)
     if (bestPallets.empty())
@@ -100,6 +75,37 @@ void bruteForce(const Truck &truck, const std::vector<Pallet> &pallets)
               << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count()
               << " ms\n";
 
+    std::cout << "Maximum profit: " << maxProfit << "\n";
+
     // Print the pallets selected as the optimal solution
     printPalletDetails(bestPallets, pallets, truck);
+}
+
+void backtrack(const std::vector<Pallet> &pallets, int maxWeight, int index, int currentWeight,
+               int currentProfit,
+               std::vector<Pallet> &currentPallets, std::vector<Pallet> &bestPallets,
+               int &maxProfit)
+{
+    // Base case: if the current index is equal to the number of pallets
+    if (index == pallets.size())
+    {
+        if (currentProfit > maxProfit ||
+            (currentProfit == maxProfit && currentPallets.size() < bestPallets.size()))
+
+        {
+            maxProfit = currentProfit;
+            bestPallets = currentPallets;
+        }
+        return;
+    }
+    backtrack(pallets, maxWeight, index + 1, currentWeight, currentProfit, currentPallets, bestPallets, maxProfit);
+    int newWeight = currentWeight + pallets[index].weight;
+    if (newWeight <= maxWeight)
+    {
+        currentPallets.push_back(pallets[index]);
+        backtrack(pallets, maxWeight, index + 1, newWeight,
+                  currentProfit + pallets[index].profit,
+                  currentPallets, bestPallets, maxProfit);
+        currentPallets.pop_back();
+    }
 }
