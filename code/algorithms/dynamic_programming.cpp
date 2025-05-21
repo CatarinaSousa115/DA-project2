a6859c14516661f50fe5ae5ee65875fb2562f45c
CatarinaSousa115 <up202306419@edu.fe.up.pt>
2025-05-21 02:35:31 +0100

    dynamic

--- code/algorithms/dynamic_programming.cpp
+++ code/algorithms/dynamic_programming.cpp
@@ -8,83 +8,68 @@
 
 void dynamicProgramming(const Truck &truck, const std::vector<Pallet> &pallets)
 {
-    int weightMax = truck.capacity;
-}
+    std::cout << "\n=== Dynamic Programming Algorithm ===\n";
+    auto start = std::chrono::high_resolution_clock::now();
 
+    int maxWeight = truck.capacity;
+    int n = pallets.size();
 
-unsigned int knapsackDP(unsigned int values[], unsigned int weights[], unsigned int n, unsigned int maxWeight, bool usedItems[]) {
-    unsigned int maxValue[100][101]; // Assumindo n <= 100, maxWeight <= 100
+    std::vector<int> bestProfitAtWeight(maxWeight + 1, 0);
+    std::vector<int> palletCountAtWeight(maxWeight + 1, INT_MAX);
+    std::vector<int> previousWeight(maxWeight + 1, -1);
+    std::vector<int> chosenPalletIndex(maxWeight + 1, -1);
+    palletCountAtWeight[0] = 0;
 
-    for(unsigned int k = 0; k <= maxWeight; k++) {
-        maxValue[0][k] = (k >= weights[0]) ? values[0] : 0;
+    if (pallets.empty() || maxWeight <= 0)
+    {
+        std::cout << "No pallets available or 0 capacity" << std::endl;
+        return;
     }
 
-    for(unsigned int i = 1; i <= n; i++) {
-        maxValue[i][0] = 0;
-    }
-
-    for(unsigned int i = 1; i < n; i++) {
-        for(unsigned int k = 1; k <= maxWeight; k++) {
-            if(k < weights[i]) {
-                maxValue[i][k] = maxValue[i - 1][k];
-            } else {
-                unsigned int withItem = maxValue[i - 1][k - weights[i]] + values[i];
-                unsigned int withoutItem = maxValue[i - 1][k];
-                maxValue[i][k] = std::max(withItem, withoutItem);
+    for (int i = 0; i < n; ++i)
+    {
+        int weight = pallets[i].weight;
+        int profit = pallets[i].profit;
+        for (int w = maxWeight; w >= 0; --w)
+        {
+            if (w >= weight && chosenPalletIndex[w - weight] != i)
+            {
+                int prevW = w - weight;
+                int newProfit = bestProfitAtWeight[prevW] + profit;
+                int newusedPallets = palletCountAtWeight[prevW] + 1;
+                if (newProfit > bestProfitAtWeight[w] || (newProfit == bestProfitAtWeight[w] && newusedPallets < palletCountAtWeight[w]))
+                {
+                    bestProfitAtWeight[w] = newProfit;
+                    palletCountAtWeight[w] = newusedPallets;
+                    previousWeight[w] = prevW;
+                    chosenPalletIndex[w] = i;
+                }
             }
         }
     }
+    int maxProfit = bestProfitAtWeight[maxWeight];
 
-    for(unsigned int i = 0; i < n; i++) {
-        usedItems[i] = false;
-    }
+    std::vector<Pallet> usedMaxProfitPallets;
+    std::vector<bool> used(n, false);
+    int w = maxWeight;
 
-    unsigned int remainingWeight = maxWeight;
-    for(int i = n - 1; i > 0; i--) {
-        if(maxValue[i][remainingWeight] != maxValue[i - 1][remainingWeight]) {
-            usedItems[i] = true;
-            remainingWeight -= weights[i];
+    while (w > 0 && chosenPalletIndex[w] != -1)
+    {
+        int idx = chosenPalletIndex[w];
+        if (!used[idx]) {
+            usedMaxProfitPallets.push_back(pallets[idx]);
+            used[idx] = true;
         }
+        w = previousWeight[w];
     }
+    std::reverse(usedMaxProfitPallets.begin(), usedMaxProfitPallets.end());
 
-    if(remainingWeight >= weights[0] && maxValue[0][remainingWeight] > 0) {
-        usedItems[0] = true;
-    }
-
-    return maxValue[n - 1][maxWeight];
-}
-
-void knapsackDPTruck(const Truck &truck, const std::vector<Pallet> &pallets)
-{
-    std::cout << "\n=== Algoritmo Programação Dinâmica ===\n";
-    auto start = std::chrono::high_resolution_clock::now();
-
-    unsigned int n = static_cast<unsigned int>(pallets.size());
-    unsigned int maxWeight = truck.capacity;
-
-    unsigned int values[100], weights[100];
-    bool usedItems[100];
-
-    for (unsigned int i = 0; i < n; ++i) {
-        values[i] = pallets[i].profit;
-        weights[i] = pallets[i].weight;
-    }
-
-    unsigned int maxProfit = knapsackDP(values, weights, n, maxWeight, usedItems);
-
-    std::cout << "Lucro máximo possível: " << maxProfit << "\n";
-    std::cout << "Paletes selecionadas:\n";
-
-    for (unsigned int i = 0; i < n; ++i) {
-        if (usedItems[i]) {
-            std::cout << "  Pallet ID: " << pallets[i].pallet
-                      << " | Peso: " << pallets[i].weight
-                      << " | Lucro: " << pallets[i].profit << "\n";
-        }
-    }
+    std::cout << "Maximum profit: " << maxProfit << "\n";
 
     auto end = std::chrono::high_resolution_clock::now();
-    std::cout << "Tempo de execução: "
+    std::cout << "Execution time: "
               << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count()
               << " ms\n";
+
+    printPalletDetails(usedMaxProfitPallets, pallets, truck);
 }
